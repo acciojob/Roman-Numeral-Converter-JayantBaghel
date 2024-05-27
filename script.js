259207b3ba428e3db12e8a8d16f907b434f99254
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-27 17:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	 if (num < 0 || num > 100000) {
+        return "Input out of range";
+    }
+    const romanSymbols = [
+        ['M', 1000], 
+        ['CM', 900],
+        ['D', 500], 
+        ['CD', 400], 
+        ['C', 100], 
+        ['XC', 90], 
+        ['L', 50], 
+        ['XL', 40], 
+        ['X', 10], 
+        ['IX', 9], 
+        ['V', 5], 
+        ['IV', 4], 
+        ['I', 1]
+    ];
+
+    let result = '';
+    
+    for (let [symbol, value] of romanSymbols) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
 
   //your code here
 
